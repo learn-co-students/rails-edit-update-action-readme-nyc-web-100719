2882b463c7ef78f8f26c713589957190c926691a
ericabasak <ericabasak@gmail.com>
2019-11-03 08:06:24 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :articles, only: %i[index show new create edit update]
+
+# resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+
+# puts "creating articles"
+# get '/articles/', to: 'articles#index', as: 'articles'
+# get '/articles/new', to: 'articles#new', as: 'new_articles'
+# get '/articles/:id', to: 'articles#show', as: 'article'
+# post '/articles/', to: 'articles#create'
+# get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+# patch '/articles/:id', to: 'articles#update'
+
+# puts "articles created"
+
 end
